bdd15d337c7d9cf3312d49d16b1ce8f80bb3b291
Sebat's Mac <SebatM@Sebats-MacBook-Air.local>
2019-11-25 12:38:30 -0800

    chore: initial commit

---
+++ themes/quotes/js/api.js
@@ -0,0 +1,23 @@
+(function($){
+
+    $('#quote-button').on('click', function(event) {
+        event.preventDefault()
+        $('#quotes-content').empty();
+
+        $.ajax({
+            method: 'GET', 
+            url: wpApiSettings.root + 'wp/v2/posts?filter[orderby]=rand'
+        }).done(function(data){
+
+            const randNum = Math.floor((Math.random() * 10) + 1);
+
+            const title = data[randNum].title.rendered;
+            const content = data[randNum].content.rendered;
+
+            $('#quotes-content').html(`<h2>${title}</h2>${content}`)
+
+        })
+
+
+    })
+})(jQuery);
