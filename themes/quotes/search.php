d5be642ea29fecc5785fe4532b6fa2337bfc7fa5
Sebat's Mac <SebatM@Sebats-MacBook-Air.local>
2019-12-02 14:17:12 -0500

    style: search page

--- themes/quotes/search.php
+++ themes/quotes/search.php
@@ -1,29 +1,39 @@
 <?php get_header(); ?>
 
-<section id="about-content">
+<div class="search-grid">
+
+<section class="search-results">
+
+<h5>Search results for:</h5>
+
+<hr>
 
 <?php if( have_posts() ) :
 //The WordPress Loop: loads post content 
     while( have_posts() ) :
         the_post(); ?>
-
-        <div>
-
-            <?php the_content(); ?>
-            <h2>&mdash; <?php echo the_title();?>&comma;
-                <a class="source" href="<?php echo get_post_meta( get_the_ID(), '_qod_quote_source_url', true );?>"><span><?php echo get_post_meta( get_the_ID(), '_qod_quote_source', true );?></span></a>
-            </h2>
-
-        </div>
     
+    <div>
+        <h2><?php echo the_title();?></h2>
+        <?php the_content(); ?>
+        <br/>
+    </div>
     <!-- Loop ends -->
     <?php endwhile;?>
-
-</section>
 
 <?php else : ?>
         <p>No posts found</p>
 <?php endif;?>
 
-    
+<section class="search-button">
+    <p>1</p>
+    <p>2</p>
+    <p>Next  <i class="fas fa-long-arrow-alt-right"></i></p>
+</section>
+
+
+</section>
+
+</div>
+
 <?php get_footer();?>
